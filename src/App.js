72cc52bbd56505bbce48e54c256abff90baa7bee
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-03 22:10:50 -0700

    Seperated components into their own files

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
-//TODO: STEP 1 - Import the useState hook.
+
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import AddButtons from "./AddButtons"
+import Scoreboard from "./ScoreBoard"
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
     let [lionsPoints, addLionPoints] = useState(0);
     let [tigersPoints, addTigerPoints] = useState(0);
     let touchdown = 7;
-    let fieldGoal = 3;
+    let fieldGoal = 30;
+
+    function addPoints(teamPoints, addTeamPoints, type) {
+        return addTeamPoints(teamPoints + type)
+    }
 
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-              <div className="home__score">{lionsPoints}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-              <div className="away__score">{tigersPoints}</div>
-          </div>
+    return (
+        <div className="container">
+            <Scoreboard home={lionsPoints} away={tigersPoints}/>
+            <AddButtons tch={touchdown} fg={fieldGoal} add={addPoints} home={lionsPoints} away={tigersPoints}
+                        addHome={addLionPoints} addAway={addTigerPoints}/>
         </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-            <button className="homeButtons__touchdown" onClick={() => {
-                addLionPoints(lionsPoints += touchdown)
-            }}>Home Touchdown
-            </button>
-            <button className="homeButtons__fieldGoal" onClick={() => {
-                addLionPoints(lionsPoints += fieldGoal)
-            }}>Home Field Goal
-            </button>
-        </div>
-        <div className="awayButtons">
-            <button className="awayButtons__touchdown" onClick={() => {
-                addTigerPoints(tigersPoints += touchdown)
-            }}>Away Touchdown
-            </button>
-            <button className="awayButtons__fieldGoal" onClick={() => {
-                addTigerPoints(tigersPoints += fieldGoal)
-            }}>Away Field Goal
-            </button>
-        </div>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default App;
